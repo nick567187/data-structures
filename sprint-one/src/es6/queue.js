ba76a762d10673dcaa2056baf4638bea275ebd6e
Hack Reactor Student <student@hackreactor.com>
2018-08-01 15:18:58 -0700

    finished es6 queue and entire sprint

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,23 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(big, count, ticket, storage) {
+    this.big = 0;
+    this.count = 0;
+    this.ticket = 0;
+    this.storage = {};
   }
-
+  enqueue(value) {
+    this.storage[this.count++] = value;
+    this.big++;
+  }
+  dequeue() {
+    var temp = this.storage[this.ticket];
+    if (this.big > 0) {
+      this.big--;
+    }
+    delete this.storage[this.ticket++];
+    return temp;
+  }
+  size() {
+    return this.big;
+  }
 }
