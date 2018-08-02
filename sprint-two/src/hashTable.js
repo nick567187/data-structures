23eab11e2f40ff2f674afa71efb56783168592fb
Hack Reactor Student <student@hackreactor.com>
2018-08-02 16:04:18 -0700

    Finished Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,25 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var node = {};
+  node[k] = v;
+  if (this._storage.get(index) !== undefined) {
+    var obj = this._storage.get(index);
+    _.extend(obj, node);
+    
+  } else {
+    this._storage.set(index, node); 
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(index)[k];
 };
 
 
