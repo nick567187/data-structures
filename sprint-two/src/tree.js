c4e23a13dfb2209f936e467fa0e56269be93be10
Hack Reactor Student <student@hackreactor.com>
2018-08-01 21:40:41 -0700

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,25 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  if (this.value === target) {
+    result = true;
+  } else {
+    if (this.children.length > 0) {
+      for (var i = 0; i < this.children.length; i++) {
+        var child = this.children[i];
+        if (child.contains(target)) {
+          result = true;
+        }
+      }
+    }
+  }
+
+  return result;
 };
 
 
