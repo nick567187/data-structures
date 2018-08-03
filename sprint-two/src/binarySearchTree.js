b095b3c24b717d87c8c1e4e908fb765e0c9001d5
Hack Reactor Student <student@hackreactor.com>
2018-08-02 17:18:33 -0700

    in progress binarySearchtree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,24 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  tree.insert = function(value) {
+    var newTree = BinarySearchTree(value);
+    if (tree.left === null) {
+      if (tree.value > value) {
+        tree.left = newTree;
+      } else 
+    }
+    // if (tree.value > value) {
+    //   tree.left = newTree;
+    // } else {
+    //   tree.right = newTree;
+    // }
+  };
+  tree.contains = function() {};
+  tree.depthFirstLog = function() {};  
+  return tree;
 };
 
 
