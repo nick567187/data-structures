a84a4f3eda0e9893bf19233d49c0828a55824806
Hack Reactor Student <student@hackreactor.com>
2018-08-02 18:03:57 -0700

    finished binarySearchTree and basic sprint

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -5,19 +5,50 @@ var BinarySearchTree = function(value) {
   tree.right = null;
   tree.insert = function(value) {
     var newTree = BinarySearchTree(value);
-    if (tree.left === null) {
-      if (tree.value > value) {
+    if (tree.value > value) {
+      if (tree.left === null) {
         tree.left = newTree;
-      } else 
+      } else {
+        tree.left.insert(value);
+      }
+    } else {
+      if (tree.right === null) {
+        tree.right = newTree;
+      } else {
+        tree.right.insert(value);
+      }
     }
-    // if (tree.value > value) {
-    //   tree.left = newTree;
-    // } else {
-    //   tree.right = newTree;
-    // }
   };
-  tree.contains = function() {};
-  tree.depthFirstLog = function() {};  
+  tree.contains = function(target) {
+    var result = false;
+    if (tree.value === target) {
+      result = true;
+    } else if (tree.value > target) {
+      if(tree.left !== null) {
+        if (tree.left.contains(target)) {
+          result = true;
+        }
+      }
+    } else {
+      if (tree.right !== null) {
+        if (tree.right.contains(target)) {
+          result = true;
+        }
+      }
+    }
+    return result;
+  };
+  
+  tree.depthFirstLog = function(func) {
+    func(tree.value);
+    if (!!tree.left) {
+      tree.left.depthFirstLog(func);
+    } 
+    if (!!tree.right) {
+      tree.right.depthFirstLog(func);
+    } 
+  };  
+  
   return tree;
 };
 
