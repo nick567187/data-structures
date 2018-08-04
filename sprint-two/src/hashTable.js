da949d58698fb1c735ef014cefdcbc9b9d2c6fb3
canderson22 <charles.anderson.dev@gmail.com>
2018-08-04 10:21:11 -0700

    testing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,171 +1,78 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._counter = 0;
+  this._currentSize = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var node = [];
-  if (this._storage.get(index) !== undefined) {
-    var arr = this._storage.get(index);
-    var changed = false;
-    for (var i = 0; i < arr.length; i++) {
-      var inside = arr[i];
-      if (inside[0] === k) {
-        inside[1] = v;
-        changed = true;
-      }
-    }
-    if (changed === false) {
-      this._counter++;
-      arr.push([k, v]);
-    }
+  var index = getIndexBelowMaxForKey(k);
+  if (this._storage.get(index) === undefined) {
+    let bucket = [];
+    var tuple = [k, v];
+    bucket.push(tuple);
+    this._storage.set(index, bucket);
+    this._currentSize++;
   } else {
-    this._counter++;
-    node.push([k, v]);
-    this._storage.set(index, node);
+    let bucket = this._storage.get(index);
+    let newBucket = [];
+    bucket.forEach(tuple => {
+      if (tuple[0] === k) {
+        tuple[1] = v;
+      }
+      newBucket.push(tuple);
+      this._currentSize++;
+    });
+    this._storage.set(index, newBucket);
   }
-  if (!this._resize) {
-    // console.log('counter', this._counter);
-    this._resize = true;
-    if (this._counter >= this._limit * 0.75) {
-      console.log("up");
-      let newLimit = this._limit * 2;
-      this.resize(newLimit);
-    }
-    if (this._counter <= this._limit * 0.25) {
-      console.log("down");
-      let newLimit = this._limit / 2;
-      this.resize(newLimit);
-    }
+  if (this._currentSize > 0.75 * this._limit) {
+    this.rehash();
   }
 };
-// HashTable.prototype.reInsert = function(k, v) {
-//   var index = getIndexBelowMaxForKey(k, this._limit);
-//   var node = [];
-//   if (this._storage.get(index) !== undefined) {
-//     var arr = this._storage.get(index);
-//     var changed = false;
-//     for (var i = 0; i < arr.length; i++) {
-//       var inside = arr[i];
-//       if (inside[0] === k) {
-//         inside[1] = v;
-//         changed = true;
-//       }
-//     }
-//     if (changed === false) {
-//       arr.push([k, v]);
-//     }
-//   } else {
-//     node.push([k, v]);
-//     this._counter++;
-//     this._storage.set(index, node);
-//   }
-// };
+
+HashTable.prototype.remove = function() {};
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var arr = this._storage.get(index);
-  for (var i = 0; i < arr.length; i++) {
-    var inside = arr[i];
-    if (inside[0] === k) {
-      return inside[1];
+  var index = getIndexBelowMaxForKey(k);
+  var bucket = this._storage.get(index);
+  for (let i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
+  return undefined;
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var arr = this._storage.get(index);
-  for (var i = 0; i < arr.length; i++) {
-    var inside = arr[i];
-    if (inside[0] === k) {
-      arr.splice(i, 1);
-      this._counter--;
-    }
-  }
-  if (!this._resize) {
-    if (this._counter <= this._limit * 0.25) {
-      console.log("down");
-      let newLimit = this._limit / 2;
-      this.resize(newLimit);
-    }
-  }
-};
-
-HashTable.prototype.resize = function(newLimit) {
-  var oldStorage = this._storage;
-
-  // this._counter = 0;
-  var values = [];
-  for (let i = 0; i < this._limit; i++) {
-    values.push(oldStorage.get(i));
-  }
-  this._limit = newLimit;
+HashTable.prototype.rehash = function(size) {
+  this._limit *= 2;
+  this._currentSize = 0;
+  let storage = this._storage;
   this._storage = LimitedArray(this._limit);
-  console.log("limit", this._limit);
-  for (let i = 0; i < values.length; i++) {
-    var val = values[i];
-    if (val !== undefined) {
-      for (let j = 0; j < val.length; j++) {
-        var subArr = val[j];
-        var k = subArr[0];
-        var v = subArr[1];
-        this.insert(k, v);
-      }
+  this._storage.each(function(bucket) {
+    if (bucket !== undefined) {
+      bucket.forEach(function(tuple) {
+        let k = tuple[0];
+        let v = tuple[1];
+        let index = getIndexBelowMaxForKey(k);
+        if (this._storage.get(index) === undefined) {
+          let bucket = [];
+          var tuple = [k, v];
+          bucket.push(tuple);
+          this._storage.set(index, bucket);
+          this._currentSize++;
+        } else {
+          let bucket = this._storage.get(index);
+          let newBucket = [];
+          bucket.forEach(tuple => {
+            if (tuple[0] === k) {
+              tuple[1] = v;
+            }
+            newBucket.push(tuple);
+            this._currentSize++;
+          });
+          this._storage.set(index, newBucket);
+        }
+      });
     }
-  }
-  this._resize = false;
-  // if (this._counter >= this._limit * .75) {
-  //   console.log('upupuppu');
-  //   var oldLimit = this._limit;
-  //   var oldStorage = this._storage;
-  //   var arr = [];
-  //   this._counter = 0;
-  //   this._limit = Math.ceil(oldLimit * 2);
-  //   this._storage = LimitedArray(this._limit);
-  //   for (var i = 0; i < oldLimit; i++) {
-  //     arr.push(oldStorage.get(i));
-  //   }
-  //   for (var i = 0; i < arr.length; i++) {
-  //     var elem = arr[i];
-  //     if (elem !== undefined) {
-  //       for (var j = 0; j < elem.length; j++) {
-  //         var node = elem[j];
-  //         var k = node[0];
-  //         var v = node[1];
-  //         this.insert(k, v);
-  //       }
-  //     }
-  //   }
-  //   this._resize = false;
-  // } else if (this._counter <= this._limit * .25 ) {
-  //   console.log('downdowndown');
-  //   var oldLimit = this._limit;
-  //   var oldStorage = this._storage;
-  //   var arr = [];
-  //   this._counter = 0;
-  //   this._limit = Math.ceil(oldLimit / 2);
-  //   this._storage = LimitedArray(this._limit);
-  //   for (var i = 0; i < oldLimit; i++) {
-  //     arr.push(oldStorage.get(i));
-  //   }
-  //   for (var i = 0; i < arr.length; i++) {
-  //     var elem = arr[i];
-  //     if (elem !== undefined) {
-  //       for (var j = 0; j < elem.length; j++) {
-  //         var node = elem[j];
-  //         var k = node[0];
-  //         var v = node[1];
-  //         this.insert(k, v);
-  //       }
-  //     }
-  //   }
-  //   this._resize = false;
-  // }
+  });
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
